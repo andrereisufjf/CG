87fe66b8fc7641877383363dea79c7e8ccf84723
Andre Reis <andreluiz.dosreis.mg@gmail.com>
2021-05-26 13:52:04 -0300

    correcao index.html

--- htmlNovo/resources/list.js
+++ htmlNovo/resources/list.js
@@ -1,66 +1,66 @@
 var list = {
-	"en": {
-		"Projects": {
-			"Old Site":  {
-				"Site Base Disciplina": "indexAntigo.html",
-			},
-			"Aula 01":  {
-				"Ex 01": "html/aula01_ex01",
-				"Using the keyboard": "html/keyboardExampleInfo",
-				"Geometric Transformations 1": "html/geometricTransformationInfo",
-				"Geometric Transformations 2": "html/geometricTransformation2Info",
-				"Projection": "html/projectionInfo",
-				"Virtual Camera": "html/virtualCameraInfo",
-			},
-			"Animations":  {
-				"Basic": "html/animationInfo",
-				"Using External Objects": "html/animationExternalObjectsInfo",
-			},
-			"Geometries":  {
-				"Basic": "html/geometriesBasicInfo",
-				"Lathe": "html/geometriesLatheInfo",
-				"Extrusion": "html/geometriesExtrusionInfo",
-				"Convex": "html/geometriesConvexInfo",
-				"Loading External Files": "html/geometriesExternalObjectsInfo",
-			},
-			"Lighting":  {
-				"Types of lights": "html/lightingTypesInfo",
-				"Types of materials": "html/lightingMaterialsInfo",
-				"Spotlight in Details": "html/lightingSpotlightDetailsInfo",
-			},
-			"Textures":  {
-				"Basic": "html/textureBasicInfo",
-				"Main Options": "html/textureOptionsInfo",
-				"Textured External Objects": "html/textureExternalObjectsInfo",
-			},
-			"Raytracing":  {
-				"Example 1": "html/raytracingExample1Info",
-				"Example 2": "html/raytracingExample2Info",
-			},
-		},
-		"XR": {
-			"AR & VR with external libs":  {
-				"VR - Primitives": "html/VR_BasicInfo",
-				"VR - Panorama": "html/VR_PanoramaInfo",
-				"VR - External Models": "html/VR_ExternalModelInfo",
-				"AR - Fiducial Marker": "html/AR_BasicInfo",
-			},
-			"AR & VR with WebXR":  {
-				"VR - Panorama": "html/webxr_VR_PanoramaInfo",
-				"VR - Teleport": "html/webxr_VR_TeleportInfo",
-				"VR - Dragging": "html/webxr_VR_DraggingInfo",
-				"VR - Labelling":"html/webxr_VR_LabellingInfo",
-				"AR - Hit and Build": "html/webxr_AR_HitAndBuildInfo",
-			},
+    "en": {
+        "Projects": {
+            "Old Site": {
+                "Site Base Disciplina": "/indexAntigo.html",
+            },
+            "Aula 01": {
+                "Ex 01": "htmlNovo/aula01_ex01",
+                "Using the keyboard": "html/keyboardExampleInfo",
+                "Geometric Transformations 1": "html/geometricTransformationInfo",
+                "Geometric Transformations 2": "html/geometricTransformation2Info",
+                "Projection": "html/projectionInfo",
+                "Virtual Camera": "html/virtualCameraInfo",
+            },
+            "Animations": {
+                "Basic": "html/animationInfo",
+                "Using External Objects": "html/animationExternalObjectsInfo",
+            },
+            "Geometries": {
+                "Basic": "html/geometriesBasicInfo",
+                "Lathe": "html/geometriesLatheInfo",
+                "Extrusion": "html/geometriesExtrusionInfo",
+                "Convex": "html/geometriesConvexInfo",
+                "Loading External Files": "html/geometriesExternalObjectsInfo",
+            },
+            "Lighting": {
+                "Types of lights": "html/lightingTypesInfo",
+                "Types of materials": "html/lightingMaterialsInfo",
+                "Spotlight in Details": "html/lightingSpotlightDetailsInfo",
+            },
+            "Textures": {
+                "Basic": "html/textureBasicInfo",
+                "Main Options": "html/textureOptionsInfo",
+                "Textured External Objects": "html/textureExternalObjectsInfo",
+            },
+            "Raytracing": {
+                "Example 1": "html/raytracingExample1Info",
+                "Example 2": "html/raytracingExample2Info",
+            },
+        },
+        "XR": {
+            "AR & VR with external libs": {
+                "VR - Primitives": "html/VR_BasicInfo",
+                "VR - Panorama": "html/VR_PanoramaInfo",
+                "VR - External Models": "html/VR_ExternalModelInfo",
+                "AR - Fiducial Marker": "html/AR_BasicInfo",
+            },
+            "AR & VR with WebXR": {
+                "VR - Panorama": "html/webxr_VR_PanoramaInfo",
+                "VR - Teleport": "html/webxr_VR_TeleportInfo",
+                "VR - Dragging": "html/webxr_VR_DraggingInfo",
+                "VR - Labelling": "html/webxr_VR_LabellingInfo",
+                "AR - Hit and Build": "html/webxr_AR_HitAndBuildInfo",
+            },
 
-		},
-		"Additional Examples":{
-			"Misc":  {
-				"Group Geometry": "html/exampleGroupInfo",
-				"First Person": "html/exampleFirstPersonInfo",
-				"Drag Control": "html/exampleDragControlInfo",
-				"Audio Example": "html/exampleAudioInfo",
-			},
-		},
-	},
+        },
+        "Additional Examples": {
+            "Misc": {
+                "Group Geometry": "html/exampleGroupInfo",
+                "First Person": "html/exampleFirstPersonInfo",
+                "Drag Control": "html/exampleDragControlInfo",
+                "Audio Example": "html/exampleAudioInfo",
+            },
+        },
+    },
 };
